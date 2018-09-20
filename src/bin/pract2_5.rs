6fd3b6d34a742eedc6fb80fce6d678e15a43ffc1
VentGrey <omarpurataf@gmail.com>
2018-09-19 22:07:19 -0500

    se aterminó la práctica 2.5

--- src/bin/pract2_5.rs
+++ src/bin/pract2_5.rs
@@ -1,10 +1,30 @@
 use std::io;
 
 fn main() {
-    println!("*****------------------*****");
-    println!("*");
+    const P_KM:f64 = 0.23;
+
     let mut entrada = String::new();
     println!("* Ingrese los km del viaje *");
     io::stdin().read_line(&mut entrada).unwrap();
+
+    let mut km:f64 = entrada.parse().unwrap();
+
+    let mut entrada1 = String::new();
+    println!("* Ingrese el tiempo de estancia *");
+    io::stdin().read_line(&mut entrada1).unwrap();
+
+    let dias:i32 = entrada1.parse().unwrap();
+
+    if dias <= 0 || km <=  0.0 {
+        panic!("._. ¿entonces para que quieres viajar?");
+    } else if dias > 7 && km > 800.00 {
+        km = km * P_KM;
+        km = km - (km * 0.30);
+        println!("Su precio total a pagar es de: ${}", km);
+    }
+
+    km = km * P_KM;
+    println!("Su precio a pagar es de ${}", km);
+
 
 }
