d416c3ae89ba6fbc534a48e036e6e643ed1c3e7c
VentGrey <omarpurataf@gmail.com>
2018-09-23 16:34:06 -0500

    nueva práctica

---
+++ src/bin/pract2_8.rs
@@ -0,0 +1,20 @@
+/* Elabore un programa que dado como dato una temperatura en grados Fah-
+*  renheit, determine el deporte que es apropiado practicar a esa temperatura,
+*  teniendo en cuenta la siguiente tabla:
+
+Tabla
+---------------------------------
+Natación |      Temperatura > 85|
+Tenis    | 70 < Temperatura ≤ 85|
+Golf     | 32 < Temperatura ≤ 70|
+Esquí    | 10 < Temperatura ≤ 32|
+Marcha   |      Temperatura ≤ 10|
+---------------------------------
+*/
+
+fn main() {
+    let mut entrada = String::new();
+    println!("Ingrese la temperatura actual");
+    io::stdin().read_line(&mut)
+
+}
