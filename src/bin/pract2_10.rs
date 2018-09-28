30e5a43ba329a4501e683e63821aae5d9b3b5d5d
VentGrey <omarpurataf@gmail.com>
2018-09-28 17:35:46 -0500

    se agregó la práctica 2.10

---
+++ src/bin/pract2_10.rs
@@ -0,0 +1,33 @@
+/* Elabore un programa que permita realizar operaciones aritméticas ele men-
+* tales, según la clave ingresada.
+|Clave | Operación |
+|  +   | suma
+|  -   | resta
+|  *   | multiplicación
+|  /   | división
+----------------- */
+
+use std::io;
+
+fn main() {
+    let mut entrada = String::new();
+    println!("Ingrese el primer número");
+    io::stdin().read_line(&mut entrada).unwrap();
+
+    let mut entrada1 = String::new();
+    println!("Ingrese el segundo número");
+    io::stdin().read_line(&mut entrada1).unwrap();
+
+    let num1:f64 = entrada.parse().unwrap();
+    let num2:f64 = entrada1.parse().unwrap();
+
+    let mut operador = io::stdin().read_line(&mut operador).unwrap();
+
+    match operador {
+        '+' => println!("La suma de los números es: {}", num1 + num2);
+        '-' => println!("La diferencia de los números es: {}", num1 - num2);
+        '*' => println!("El producto resultante es: {}", num1 * num2);
+        '/' => println!("La división resultante es: {}", num1 / num2);
+        _ => println!("Ingrese un operador válido");
+    }
+}
