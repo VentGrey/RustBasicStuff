5ecbfdb210d0aac2e33fa1511d2d35ecc0ec43df
VentGrey <omarpurataf@gmail.com>
2018-09-28 16:54:25 -0500

    se agregó la práctica 2.8

--- src/bin/pract2_8.rs
+++ src/bin/pract2_8.rs
@@ -11,10 +11,24 @@ Esquí    | 10 < Temperatura ≤ 32|
 Marcha   |      Temperatura ≤ 10|
 ---------------------------------
 */
+use std::io;
 
 fn main() {
     let mut entrada = String::new();
     println!("Ingrese la temperatura actual");
-    io::stdin().read_line(&mut)
+    io::stdin().read_line(&mut entrada).unwrap();
 
+    let temperatura:f64 = entrada.parse().unwrap();
+
+    if temperatura > 85.00 {
+        println!("Deberías practicar natación");
+    } else if temperatura > 70.00 && temperatura <= 85.00 {
+        println!("Deberías practicar Tenis");
+    } else if temperatura > 32.00 && temperatura <= 70.00 {
+        println!("Deberias practicar golf");
+    } else if temperatura > 10.00 && temperatura <= 32.00 {
+        println!("Deberías practicar esquí");
+    } else if temperatura <= 10.00 {
+        println!("Deberías practicar marcha");
+    }
 }
