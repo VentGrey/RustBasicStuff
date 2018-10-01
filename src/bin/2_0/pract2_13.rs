78964fea36edf51745b027701c5cd64efc93ef77
VentGrey <omarpurataf@gmail.com>
2018-09-30 21:23:53 -0500

    avance en la práctica 2.13

--- src/bin/2_0/pract2_13.rs
+++ src/bin/2_0/pract2_13.rs
@@ -12,20 +12,81 @@ fn main() {
     println!("25 - - Africa");
     println!("29 - - Oceanía");
     io::stdin().read_line(&mut entrada).unwrap();
-
     let clave:i16 = entrada.parse().unwrap();
 
     let mut entrada2 = String::new();
     println!("Ingrese el número de minutos de la llamada");
     io::stdin().read_line(&mut entrada2).unwrap();
 
-    let mins:i32 = entrada2.parse().unwrap();
+    let mins:f64 = entrada2.parse().unwrap();
+    let mut precio:f64 = 0.0;
 
+    //Desmadre de llaves incoming
+    //Brace yourselves!
     match clave {
         12 => {
-            if mins >= 3 {
-                
+            if mins >= 3.0 {
+                precio = 2.0;
+                precio = precio * mins;
+            } else if mins <= 4.0 {
+                precio = 1.5;
+                precio = precio * mins;
             }
         }
+        15 => {
+            if mins >= 3.0 {
+                precio = 2.2;
+                precio = precio * mins;
+            } else if mins <= 4.0 {
+                precio = 1.8;
+                precio = precio * mins;
+            }
+        }
+        18 => {
+                if mins >= 3.0 {
+                    precio = 4.5;
+                    precio = precio * mins;
+                } else if mins <= 4.0 {
+                precio = 3.5;
+                precio = precio * mins;
+            }
+        }
+        19 => {
+                if mins >= 3.0 {
+                    precio = 3.5;
+                    precio = precio * mins;
+            } else if mins <= 4.0 {
+                    precio = 2.7;
+                    precio = precio * mins;
+            }
+        }
+        23 => {
+                if mins >= 3.0 {
+                precio = 6.0;
+                precio = precio * mins;
+            } else if mins <= 4.0 {
+                precio = 4.6;
+                precio = precio * mins;
+            }
+        }
+        25 => {
+                if mins >= 3.0 {
+                    precio = 6.0;
+                    precio = precio * mins;
+            } else if mins <= 4.0 {
+                    precio = 4.6;
+                    precio = precio * mins;
+            }
+        }
+        29 => {
+                if mins >= 3.0 {
+                    precio = 3.5;
+                    precio = precio * mins;
+            } else if mins <= 4.0 {
+                    precio = 2.7;
+                    precio = precio * mins;
+            }
+        }
+
     }
 }
