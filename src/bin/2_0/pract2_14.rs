7adf436b77815be2111b05d9d22837ceb899eee8
VentGrey <omarpurataf@gmail.com>
2018-10-19 14:50:06 -0500

    se agregó la práctica 2.14

---
+++ src/bin/2_0/pract2_14.rs
@@ -0,0 +1,20 @@
+/*
+Los clientes de un hospital tienen una credencial en la que además de otra
+información registra una categorı́a que depende de los ingresos económicos del
+núcleo familiar del paciente (cliente). Teniendo en cuenta la categorı́a el hospital
+les hace un descuento cuando tienen que pagar su cuenta, con base en la siguiente
+tabla:
+
+Categoría   Descuento
+    1           35%
+    2           22%
+    3           15%
+    4           5%
+
+Observe que las categorías mayores a 4 no tienen descuento
+*/
+use std::io;
+
+fn main() {
+    
+}
