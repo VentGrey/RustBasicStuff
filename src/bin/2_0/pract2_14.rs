3affef7b5f83f55a65d528f19be82955f618b2ac
VentGrey <omarpurataf@gmail.com>
2018-10-19 20:15:53 -0500

    se terminó la práctica 2.14

--- src/bin/2_0/pract2_14.rs
+++ src/bin/2_0/pract2_14.rs
@@ -16,5 +16,23 @@ Observe que las categorías mayores a 4 no tienen descuento
 use std::io;
 
 fn main() {
-    
+    let mut entrada = String::new();
+    println!("Bienvenido al hospital, ingrese la categoría de su tarjeta");
+    io::stdin().read_line(&mut entrada).unwrap();
+
+    let credencial:i8 = entrada.parse().unwrap();
+
+    let mut entrada2 = String::new();
+    println!("Ingrese el monto en tu ticket");
+    io::stdin().read_line(&mut entrada2).unwrap();
+
+    let monto:f64 = entrada2.parse().unwrap();
+
+    match credencial {
+        1 => println!("Su monto a pagar es de: ${}", monto - (monto * 0.35)),
+        2 => println!("Su monto a pagar es de: ${}", monto - (monto * 0.22)),
+        3 => println!("Su monto a pagar es de: ${}", monto - (monto * 0.15)),
+        4 => println!("Su monto a pagar es de: ${}", monto - (monto * 0.05)),
+        _ => println!("Usted no recibe descuento, pague {}", monto),
+    }
 }
