67c24adac3fd4cb2c82834cfd6b0aa9222ead32c
VentGrey <omarpurataf@gmail.com>
2018-11-13 21:01:28 -0600

    Corrected practice 2.1

--- src/bin/2_0/pract2_1.rs
+++ src/bin/2_0/pract2_1.rs
@@ -15,20 +15,20 @@ fn main() {
     if angle.cos() != 0.0 {
         let tang:f64 = angulo.sin() / angulo.cos();
         println!("Tangent is equal to: {}", tang);
-<<<<<<< HEAD
+
         if angle.sin() != 0.0 {
             let cot:f64 = angle.cos() / angle.sin();
-=======
+
         if angulo.sin() != 0.0 {
             let cot:f64 = angulo.cos() / angulo.sin();
->>>>>>> ee6451a32f51e78b0d5f224424058e3ce9446565
+
             println!("Cotangent is equal to: {}", cot);
         } else {
-            println!("Angle sin cannot be zero >:c");
-            process::exit(1);
+            panic!("Angle sin cannot be zero >:c");
+
         }
     } else {
-        println!("Cosine must be different from zero >:c");
-        process::exit(1);
+        panic!("Cosine must be different from zero >:c");
+
     }
 }
