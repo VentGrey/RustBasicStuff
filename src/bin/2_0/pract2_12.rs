4737b19ed9422c6277e3fd0c1aa3233e96bed33f
VentGrey <omarpurataf@gmail.com>
2018-11-29 13:10:37 -0600

    Translated 2.11.rs

--- src/bin/2_0/pract2_12.rs
+++ src/bin/2_0/pract2_12.rs
@@ -25,24 +25,24 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    println!("Ingrese la cantidad de unidades que desea convertir");
+    println!("Enter the amount of units you wish to convert");
     io::stdin().read_line(&mut input).unwrap();
 
     let med:f64 = input.parse().unwrap();
 
     let mut input_1 = String::new();
-    println!("Ingrese la conversión deseada: ");
-    println!("1° Pulgadas a Milímetros");
-    println!("2° Yardas a Metros");
-    println!("3° Millas a Kilómetros");
+    println!("Input your conversion option: ");
+    println!("1° Inches to Milimeters");
+    println!("2° Yards to Meters");
+    println!("3° Miles to Kilometers");
     io::stdin().read_line(&mut input_1).unwrap();
 
     let op:i8 = input_1.parse().unwrap();
 
     match op {
-        1 => println!("{} Pulgadas equivalen a {} Milímetros", med, med * 25.40),
-        2 => println!("{} Yardas equivalen a {} Metros", med, med * 0.9144),
-        3 => println!("{} Millas equivalen a {} Kilómetros", med, med * 1.6093),
-        _ => panic!("ingrese un operador válido!"),
+        1 => println!("{} Inches are {} Milimeters", med, med * 25.40),
+        2 => println!("{} Yards are {} Meters", med, med * 0.9144),
+        3 => println!("{} Miles are {} Kilometers", med, med * 1.6093),
+        _ => panic!("Please insert a valid operator!"),
     }
 }
