ec7c48d55b6b6696c04a80dbdd16243cf4fa6e44
VentGrey <omarpurataf@gmail.com>
2018-11-29 13:11:59 -0600

    Minor changes

--- src/bin/2_0/pract2_12.rs
+++ src/bin/2_0/pract2_12.rs
@@ -1,26 +1,3 @@
-/*
- * Elabore un programa que permita convertir de pulgadas a milı́metros, de
- * yardas a metros y de millas a kilómetros.
-
- * Datos:
-   OPCION es una variable de tipo entero que representa el tipo de conversión
-   que se desea realizar.
-
-   Se ingresa:
-   1. Para convertir de pulgadas a milı́metros.
-   2. Para convertir de yardas a metros.
-   3. Para convertir de millas a kilómetros.
-
-   Variables:
-   MED es una variable de tipo real que representa la medida que se ingresará pa-
-   ra conversión.
-
-Consideraciones
-1 pulgada equivale a 25.40 milímetros.
-1 yarda equivale a 0.9144 metros.
-1 milla equivale a 1.6093 kilómetros.
-*/
-
 use std::io;
 
 fn main() {
