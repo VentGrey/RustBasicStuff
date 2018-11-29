cff19811fe94c1181501ba42b2f2ba618cf449c1
VentGrey <omarpurataf@gmail.com>
2018-11-29 13:30:12 -0600

    Translated

--- src/bin/2_0/pract2_13.rs
+++ src/bin/2_0/pract2_13.rs
@@ -3,19 +3,19 @@ use std::io;
 fn main() {
     let mut input = String::new();
 
-    println!("Ingrese la clave de región a donde desea llamar");
-    println!("12 - - América del Norte");
-    println!("15 - - América Central" );
-    println!("18 - - América del Sur");
-    println!("19 - - Europa");
+    println!("Insert the specified region code you wish to call");
+    println!("12 - - North America");
+    println!("15 - - Center America" );
+    println!("18 - - South America");
+    println!("19 - - Europe");
     println!("23 - - Asia");
     println!("25 - - Africa");
-    println!("29 - - Oceanía");
+    println!("29 - - Australia");
     io::stdin().read_line(&mut input).unwrap();
     let clave:i16 = input.parse().unwrap();
 
     let mut input_2 = String::new();
-    println!("Ingrese el número de minutos de la llamada");
+    println!("Input your call minutes");
     io::stdin().read_line(&mut input_2).unwrap();
 
     let mins:f64 = input_2.parse().unwrap();
@@ -24,87 +24,86 @@ fn main() {
     //Desmadre de llaves incoming
     //Brace yourselves!
     //
-    //MAS DE 3 NIVELES DE INDENT! El código está mal :(
     match clave {
         12 => {
                 if mins >= 3.0 {
                     precio = 2.0;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else if mins <= 4.0 {
                     precio = 1.5;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else {
-                    panic!("Ocurrió un eror!");
+                    panic!("error!");
                 }
         }
         15 => {
                 if mins >= 3.0 {
                     precio = 2.2;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else if mins <= 4.0 {
                     precio = 1.8;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else {
-                    panic!("Ocurrió un eror!");
+                    panic!("error!");
                 }
         }
         18 => {
                 if mins >= 3.0 {
                     precio = 4.5;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else if mins <= 4.0 {
                     precio = 3.5;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else {
-                    panic!("Ocurrió un eror!");
+                    panic!("Something is wrong!!");
                 }
         }
         19 => {
                 if mins >= 3.0 {
                     precio = 3.5;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else if mins <= 4.0 {
                     precio = 2.7;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else {
-                    panic!("Ocurrió un eror!");
+                    panic!("Something is wrong!!");
                 }
         }
         23 | 25 => {
                 if mins >= 3.0 {
                     precio = 6.0;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else if mins <= 4.0 {
                     precio = 4.6;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else {
-                    panic!("Ocurrió un eror!");
+                    panic!("Something is wrong!!");
                 }
         }
         29 => {
                 if mins >= 3.0 {
                     precio = 5.0;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else if mins <= 4.0 {
                     precio = 3.9;
                     precio = precio * mins;
-                    println!("Su importe total es de: ${} pesos", precio);
+                    println!("You have to pay ${}", precio);
                 } else {
-                    panic!("Ocurrió un eror!");
+                    panic!("Something is wrong!!");
                 }
         }
-        _ => panic!("Ingrese una opción válida!");
+        _ => panic!("Invalid option");
 
     }
 }
