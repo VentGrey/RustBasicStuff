1f791d9ea377129a51d6ad5d47e56588364172b3
VentGrey <omarpurataf@gmail.com>
2018-12-17 01:08:51 -0600

    Added description

--- src/pract1_1.rs
+++ src/pract1_1.rs
@@ -1,7 +1,11 @@
+/* Write a program that takes two numbers from the user input
+and calculates the addition, substraction and division of both
+numbers */
+
 use std::io;
 
 fn main() {
-    //Procesamiento de la primer variable
+    //First Variable Processind
     let mut input = String::new();
     println!("Enter the first number");
 
@@ -19,3 +23,19 @@ fn main() {
     println!("The product of the previous numbers is: {}", num1 * num2);
     println!("The division of the previous numbers results in: {}", num1 / num2);
 }
+
+/*
+fn main() {
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
+
+    let trimmed = input_text.trim();
+    match trimmed.parse::<u32>() {
+        Ok(i) => println!("your integer input: {}", i),
+        Err(..) => println!("this was not an integer: {}", trimmed),
+    };
+}
+
+*/
