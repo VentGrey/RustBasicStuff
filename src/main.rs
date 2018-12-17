1d292d86f6a8161034a8d4013644b51b3c1dbe10
VentGrey <omarpurataf@gmail.com>
2018-12-17 01:32:22 -0600

    Exercices menu workout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+//Modules spam, brace yourselves!
+
+
+
 use std::io;
 
-fn main() {}
+fn main() {
+    println!("----------Menu-------------------------------------------------");
+    println!("Welcome to the main menu");
+    println!("---------------------------------------------------------------");
+    println!("These are the current programs in the project, status are\
+    reported at the right");
+    println!("Please select your desired practices folder:");
+
+    let mut scanner = String::new();
+    io::stdin()
+        .read_line(&mut scanner)
+        .expect("Failed to read from stdin library");
+
+    let option = scanner.trim();
+    match option.parse::<i8>() {
+        Ok(i) => {
+
+        },
+        Err(e) => {
+            println!("Error: {}", e.to_string());
+        },
+    }
+}
