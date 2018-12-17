b8bd6fbd604edcacaee44a11547c8e2d938c8d60
VentGrey <omarpurataf@gmail.com>
2018-12-17 02:17:15 -0600

    Started building menu

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,32 @@ mod pract1_3;
 mod pract1_4;
 mod pract1_5;
 
-
-
-
 use std::io;
 
 fn main() {
     println!("----------Menu-------------------------------------------------");
     println!("Welcome to the main menu");
     println!("---------------------------------------------------------------");
-    println!("These are the current programs in the project, status are\
-    reported at the right");
-    println!("Please select your desired practices folder:");
+    println!(
+        "These are the current programs in the project, status are\
+         reported at the right"
+    );
+    println!("------FIRST UNIT-----");
+    println!("(1) 1.1 => Basic aritmethics [IN PROGRESS]");
+    println!("Please select your desired practice to execute:");
 
-    let mut scanner = String::new();
+    let mut usr_input = String::new();
     io::stdin()
-        .read_line(&mut scanner)
+        .read_line(&mut usr_input)
         .expect("Failed to read from stdin library");
 
-    let option = scanner.trim();
+    let option = usr_input.trim();
     match option.parse::<i8>() {
-        Ok(i) => {
-
-        },
+        Ok(i) => {}
         Err(e) => {
+            println!("This is not an integer {}", option);
+            println!("Specific error report:");
             println!("Error: {}", e.to_string());
-        },
+        }
     }
 }
