cc0692c851a673f64aeb2392eacac42148c54191
VentGrey <omarpurataf@gmail.com>
2018-12-17 20:30:10 -0600

    Missing semicolons e.e

--- src/pract2_9.rs
+++ src/pract2_9.rs
@@ -5,19 +5,19 @@ fn main() {
     println!("Input the first number");
     io::stdin().read_line(&mut input).unwrap();
 
-    let num1:f64 = input.parse().unwrap()
+    let num1:f64 = input.parse().unwrap();
 
     let mut input_1 = String::new();
     println!("Input the second number");
     io::stdin().read_line(&mut input_1).unwrap();
 
-    let num2:f64 = input_1.parse().unwrap()
+    let num2:f64 = input_1.parse().unwrap();
 
     let mut input_2 = String::new();
     println!("Input the third number");
     io::stdin().read_line(&mut input_2).unwrap();
 
-    let num3:f64 = input_2.parse().unwrap()
+    let num3:f64 = input_2.parse().unwrap();
 
     if num1 > num2 && num1 > num3 {
         println!("Number {} is bigger than {} y {}", num1, num2, num3);
