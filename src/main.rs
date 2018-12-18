f4d13b507f9900e6a816be59b93a4da431a2ad59
VentGrey <omarpurataf@gmail.com>
2018-12-17 20:06:48 -0600

    Added all  current modules

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,24 @@ mod pract1_2;
 mod pract1_3;
 mod pract1_4;
 mod pract1_5;
+//-- 2nd Part Modules
+mod pract2_1;
+mod pract2_2;
+mod pract2_3;
+mod pract2_4;
+mod pract2_5;
+mod pract2_6;
+mod pract2_7;
+mod pract2_8;
+mod pract2_9;
+mod pract2_10;
+mod pract2_11;
+mod pract2_12;
+mod pract2_13;
+mod pract2_14;
+//-- 3rd Part Modules
+mod pract3_1;
+
 
 use std::io;
 
@@ -16,6 +34,7 @@ fn main() {
         "These are the current programs in the project, status are\
          reported at the right"
     );
+    //TODO: Finish menu
     println!("------FIRST UNIT-----");
     println!("(1) 1.1 => Basic aritmethics [IN PROGRESS]");
     println!("Please select your desired practice to execute:");
