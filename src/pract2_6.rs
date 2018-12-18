44ad619642f867ea8243be9e760264a3ef2f5317
VentGrey <omarpurataf@gmail.com>
2018-12-18 01:05:40 -0600

    Fixed most warns

--- src/pract2_6.rs
+++ src/pract2_6.rs
@@ -8,11 +8,11 @@ fn main() {
     let mut salary:f64 = input.parse().unwrap();
 
     if salary < 10000.00 {
-    	salary = salary * 1.15;
+    	salary *= 1.15;
     } else if salary >= 10000.00 {
-    	salary = salary * 1.11;
+    	salary *= 1.11;
     } else if salary > 15000.00 {
-    	salary = salary * 1.08;
+    	salary *= 1.08;
     } else {
     	panic!("._. You can get sued if you don't pay your workers");
     }
