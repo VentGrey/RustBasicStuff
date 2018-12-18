5e4ac3cf1706820454b95f3bcf4e279d2f529e87
VentGrey <omarpurataf@gmail.com>
2018-12-18 13:13:17 -0600

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ mod pract1_4;
 mod pract1_5;
 //-- 2nd Part Modules
 mod pract2_1;
+mod pract2_10;
+mod pract2_11;
+mod pract2_12;
+mod pract2_13;
+mod pract2_14;
 mod pract2_2;
 mod pract2_3;
 mod pract2_4;
@@ -15,11 +20,6 @@ mod pract2_6;
 mod pract2_7;
 mod pract2_8;
 mod pract2_9;
-mod pract2_10;
-mod pract2_11;
-mod pract2_12;
-mod pract2_13;
-mod pract2_14;
 //-- 3rd Part Modules
 mod pract3_1;
 //-- 4th Part Modules
