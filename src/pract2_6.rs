7feeb3b170aadea2b261e06de02766adef759b92
VentGrey <omarpurataf@gmail.com>
2018-12-19 17:10:07 -0600

    removed warns

--- src/pract2_6.rs
+++ src/pract2_6.rs
@@ -1,19 +1,25 @@
+use colored::*;
 use std::io;
 
 pub fn main() {
     let mut input = String::new();
     println!("Input worker salary");
-    io::stdin().read_line(&mut input).unwrap();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read from STDIN");
 
-    let mut salary:f64 = input.parse().unwrap();
+    let mut salary: f64 = input.trim().parse().unwrap();
 
     if salary < 10000.00 {
-    	salary *= 1.15;
+        salary *= 1.15;
+        println!("Your total salary is {}", salary);
     } else if salary >= 10000.00 {
-    	salary *= 1.11;
+        salary *= 1.11;
+        println!("Your total salary is {}", salary);
     } else if salary > 15000.00 {
-    	salary *= 1.08;
+        salary *= 1.08;
+        println!("Your total salary is {}", salary);
     } else {
-    	panic!("._. You can get sued if you don't pay your workers");
+        panic!("._. You can get sued if you don't pay your workers");
     }
 }
