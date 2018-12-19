57596c0ed7ad714c16d4fb8455a5feb158e8e021
VentGrey <omarpurataf@gmail.com>
2018-12-19 16:06:08 -0600

    Colored all output

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,7 @@ use colored::*; // <-- Namespace violation, don't do this please
 //-- Standard imports
 use std::io;
 
+//DO NOT PASS 80 columns!
 fn main() {
     println!("----------Menu-------------------------------------------------");
     println!("Welcome to the main menu");
@@ -50,22 +51,28 @@ fn main() {
     println!("(4) 1.4 => Circle formula [{}]", "INCOMPLETE".red());
     println!("(5) 1.5 => Currency exchange [{}]", "INCOMPLETE".red());
     println!("------SECOND UNIT----");
-    println!("(6) 2.1 => Angle processing [INCOMPLETE]");
-    println!("(7) 2.2 => Basic money discount [INCOMPLETE]");
-    println!("(8) 2.3 => Basic worker's salary processing [INCOMPLETE]");
-    println!("(9) 2.4 => Floating point in fraction notation [INCOMPLETE]");
-    println!("(10) 2.5 => Basic train ticket system [INCOMPLETE]");
-    println!("(11) 2.6 => Worker salary conditionals [INCOMPLETE]");
-    println!("(12) 2.7 => Are these two numbers divisible? [INCOMPLETE]");
-    println!("(13) 2.8 => Sports practice suggestions [INCOMPLETE]");
-    println!("(14) 2.9 => The biggest of three numbers [INCOMPLETE]");
-    println!("(15) 2.10 => Advanced aritmethics [INCOMPLETE]");
-    println!("(16) 2.11 => Car insurance payments [INCOMPLETE]");
-    println!("(17) 2.12 => Distance conversion [INCOMPLETE]");
-    println!("(18) 2.13 => Call center bills management [INCOMPLETE]");
-    println!("(19) 2.14 => Hospital payment [INCOMPLETE]");
+    println!("(6) 2.1 => Angle processing [{}]", "INCOMPLETE".red());
+    println!("(7) 2.2 => Basic money discount [{}]", "INCOMPLETE".red());
+    println!("(8) 2.3 => Basic worker's salary processing\
+     [{}]", "INCOMPLETE".red());
+    println!("(9) 2.4 => Floating point in fraction notation\
+     [{}]", "INCOMPLETE".red());
+    println!("(10) 2.5 => Basic train ticket system [{}]", "INCOMPLETE".red());
+    println!("(11) 2.6 => Worker salary conditionals [{}]", "INCOMPLETE".red());
+    println!("(12) 2.7 => Are these two numbers divisible?\
+     [{}]", "INCOMPLETE".red());
+    println!("(13) 2.8 => Sports practice suggestions\
+     [{}]", "INCOMPLETE".red());
+    println!("(14) 2.9 => The biggest of three numbers\
+     [{}]", "INCOMPLETE".red());
+    println!("(15) 2.10 => Advanced aritmethics [{}]", "INCOMPLETE".red());
+    println!("(16) 2.11 => Car insurance payments [{}]", "INCOMPLETE".red());
+    println!("(17) 2.12 => Distance conversion [{}]", "INCOMPLETE".red());
+    println!("(18) 2.13 => Call center bills management\
+     [{}]", "INCOMPLETE".red());
+    println!("(19) 2.14 => Hospital payment [{}]", "INCOMPLETE".red());
     println!("------THIRD UNIT-----");
-    println!("(20) 3.1 => Consecutive sum of numbers [INCOMPLETE]");
+    println!("(20) 3.1 => Consecutive sum of numbers [{}]", "INCOMPLETE".red());
     println!("------FOURTH UNIT----");
     println!("------EXTRAS---------");
     println!("----KEEP LEARNING----");
