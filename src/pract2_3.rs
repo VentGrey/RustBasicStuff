4f43f6084fd0bffffb75773de042cbcd91843fda
VentGrey <omarpurataf@gmail.com>
2018-12-19 13:00:55 -0600

    fixed warn

--- src/pract2_3.rs
+++ src/pract2_3.rs
@@ -8,10 +8,10 @@ fn main() {
     let mut salary:f64 = input.trim().parse().unwrap();
 
     if salary < 1000.00 {
-        salary = salary * 1.15;
+        salary *= 1.15;
         print!("Your workers salary now is: ${}", salary);
     } else {
-        salary = salary * 1.12;
+        salary *= 1.12;
         print!("Your workers salary now is: ${}", salary);
     }
 }
