2dbec175b8daae4887473a7289fee12d80ea4781
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-03 02:44:42 -0500

    Added measure change exercises

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod basic;
 mod circle;
 mod divisas;
+mod cambiomedidas;
 
 use std::io;
 
@@ -10,6 +11,7 @@ fn main() {
     println!("1- Operaciones básicas");
     println!("2- Área y perímetro del círculo");
     println!("3- Cambio de divisas");
+    println!("4- Cambio de medidas");
 
     let mut entrada = String::new();
     io::stdin().read_line(&mut entrada).unwrap();
@@ -19,6 +21,7 @@ fn main() {
         1 => basic::basic_op(),
         2 => circle::circle(),
         3 => divisas::divisas(),
+        4 => cambiomedidas::cambio(),
         _ => panic!("Error de entrada D:"),
     }
 }
